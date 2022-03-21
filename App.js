b9a66592447ae10315581582ab251ea3f10db1f7
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-03-21 12:46:17 +0530

    finised

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import AddUser from './components/AddUser/AddUser';
+import UsersList from './components/AddUser/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
